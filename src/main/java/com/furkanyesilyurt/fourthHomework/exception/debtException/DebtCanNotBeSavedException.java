286092e522750ef8842c4002a88d8c728a261295
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2022-01-18 22:24:18 +0300

    Two excepitons have been added.

---
+++ src/main/java/com/furkanyesilyurt/fourthHomework/exception/debtException/DebtCanNotBeSavedException.java
@@ -0,0 +1,9 @@
+package com.furkanyesilyurt.fourthHomework.exception.debtException;
+
+public class DebtCanNotBeSavedException extends RuntimeException{
+
+    public DebtCanNotBeSavedException(String message) {
+        super(message);
+    }
+
+}
